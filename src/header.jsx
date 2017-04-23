3f250484c7faaafcd9302fe36a8bf77ea5250da7
Daniel Hess <dan9186@gmail.com>
2017-04-23 14:06:33 -0700

    updating header

--- src/header.jsx
+++ src/header.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import LogoImg from './assets/images/logo.png'
 import 'whatwg-fetch'
+import './assets/styles/header.css'
+import {Profile} from './profile.jsx'
 
 export class Header extends React.Component {
     constructor(props) {
@@ -10,19 +11,9 @@ export class Header extends React.Component {
 
     render() {
         return (
-            <header role="main-header">
+            <header className="header">
                 <div className="container">
-                    <img className="logo img-circle pull-left" src={LogoImg} alt="Logo" />
-                    <div className="identity pull-left">
-                        <h1>Daniel Hess</h1>
-                        <h4>Devops Engineer</h4>
-                    </div>
-                    <ul role="socil-icons">
-                        <li><a href="https://github.com/dan9186" target="_blank" title="Github"><i className="fa fa-2x fa-github" aria-hidden="true"></i></a></li>
-                        <li><a href="https://www.linkedin.com/in/dan9186/" target="_blank" title="linkedin"><i className="fa fa-2x fa-linkedin" aria-hidden="true"></i></a></li>
-                        <li><a href="https://twitter.com/dan9186" target="_blank" title="twitter"><i className="fa fa-2x fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="mailto:daniel@danielhess.me" title="Contact"><i className="fa fa-2x fa-envelope" aria-hidden="true"></i></a></li>
-                    </ul>
+                    <Profile name="Daniel Hess" desc="Devops Engineer" email="daniel@danielhess.me" username="dan9186"/>
                 </div>
             </header>
         )
