87afa0ec128272b4771a0b39413b968172bfe608
Daniel Hess <dan9186@gmail.com>
2017-09-29 11:36:33 -0700

    a little better formatting

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import 'whatwg-fetch'
 import './assets/styles/page.css'
+
 import {Header} from './header.jsx'
 import {Body} from './body.jsx'
 import {Footer} from './footer.jsx'
 
-ReactDOM.render(
+ReactDOM.render((
     <div>
         <Header />
         <Body />
         <Footer />
-    </div>,
-    document.getElementById('app')
-)
+    </div>
+), document.getElementById('app'))
