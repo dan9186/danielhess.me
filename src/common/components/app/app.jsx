c05a5574fd49d007c326fb48a6f10ff3dc428d10
Daniel Hess <dan9186@gmail.com>
2017-10-01 02:14:27 -0700

    adding a body section to go along header and footer

--- src/common/components/app/app.jsx
+++ src/common/components/app/app.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {Switch,Route} from 'react-router-dom'
 
-import {Footer} from '../footer/footer.jsx'
 import {Header} from '../header/header.jsx'
+import {Body} from '../body/body.jsx'
+import {Footer} from '../footer/footer.jsx'
 
 import {Home} from '../../../pages/home/home.jsx'
 import {Resume} from '../../../pages/resume/resume.jsx'
@@ -16,14 +17,7 @@ export class App extends React.Component {
         return (
             <div>
                 <Header />
-                <div className="container sections-wrapper">
-                    <div className="row">
-                        <Switch>
-                            <Route exact path='/' component={Home}/>
-                            <Route path='/resume' component={Resume}/>
-                        </Switch>
-                    </div>
-                </div>
+                <Body />
                 <Footer />
             </div>
         )
---
+++ src/common/components/body/body.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Switch,Route} from 'react-router-dom'
+
+import 'whatwg-fetch'
+
+import './style.css'
+
+import {Home} from '../../../pages/home/home.jsx'
+import {Resume} from '../../../pages/resume/resume.jsx'
+
+export class Body extends React.Component {
+    constructor(props) {
+        super()
+    }
+
+    render() {
+        return (
+            <div className="body-wrapper">
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/resume' component={Resume}/>
+                </Switch>
+            </div>
+        )
+    }
+}
