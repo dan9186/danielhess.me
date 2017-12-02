2034e314ae3bd705c1c2d2f760a2a899e4e657d9
Daniel Hess <dan9186@gmail.com>
2017-12-01 23:53:41 -0800

    taking some linting advice

--- src/common/components/app/app.jsx
+++ src/common/components/app/app.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import {Switch,Route} from 'react-router-dom'
 
 import {Header} from '../header/header.jsx'
 import {Body} from '../body/body.jsx'
 import {Footer} from '../footer/footer.jsx'
-
-import {Home} from '../../../pages/home/home.jsx'
-import {Resume} from '../../../pages/resume/resume.jsx'
 
 export class App extends React.Component {
     constructor(props) {
--- src/common/components/body/body.jsx
+++ src/common/components/body/body.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import {Switch,Route} from 'react-router-dom'
 
 import 'whatwg-fetch'
