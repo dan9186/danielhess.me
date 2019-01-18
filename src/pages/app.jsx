8845fd4a78490a80f8a4ad1f0c592937e201dfd3
Daniel Hess <dan9186@gmail.com>
2019-01-17 23:57:23 -0800

    moving app to page

---
+++ src/pages/app.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled, {createGlobalStyle} from 'styled-components'
+
+export class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <GlobalStyle />
+        <Container>
+        </Container>
+      </React.Fragment>
+    )
+  }
+}
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+    height: 100%;
+    margin: 0;
+    font-family: 'Lato', arial, sans-serif;
+    color: #434343;
+    background: #DAE3E7;
+    font-size: 16px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
