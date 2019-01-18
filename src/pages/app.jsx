d775c2bfe799b5f0f26eca0d7202dfd6e5416f00
Daniel Hess <dan9186@gmail.com>
2019-01-18 15:11:15 -0800

    main app container

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -21,11 +21,18 @@ const GlobalStyle = createGlobalStyle`
   body {
     height: 100%;
     margin: 0;
-    font-family: 'Lato', arial, sans-serif;
     color: #434343;
     background: #DAE3E7;
     font-size: 16px;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+`
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+
+  font-family: 'Lato', arial, sans-serif;
 `
