fc4ff059c2acee0eaf0aacb1a1b01c94bc7643a5
Daniel Hess <dan9186@gmail.com>
2019-01-18 22:20:24 -0800

    start of nav component

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,72 +1,57 @@
 import React from 'react'
-import {Navbar,Nav,NavItem} from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
-import {bootstrapUtils} from 'react-bootstrap/lib/utils'
 import styled from 'styled-components'
 
 import 'whatwg-fetch'
 
-bootstrapUtils.addStyle(Navbar, 'custom')
-
 export class Navigation extends React.Component {
-    constructor() {
-        super()
-    }
-
     render() {
         return (
-            <StyledNavbar fixedTop bsStyle="custom" collapseOnSelect>
-                <Navbar.Header>
-                    <StyledNavbarToggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <StyledNav>
-                        {this.render_link("/", 1, "Home")}
-                        {this.render_link("/resume", 2, "Resume")}
-                        {this.render_link("/slides", 3, "Slides")}
-                        {/*this.render_link("/blog", 4, "Blog")*/}
-                        {/*this.render_link("/projects", 5, "Projects")*/}
-                    </StyledNav>
-                </Navbar.Collapse>
-            </StyledNavbar>
+            <Navbar role="navigation">
+              <Nav>
+                {/*this.render_link("/", 1, "Home")*/}
+                {/*this.render_link("/resume", 2, "Resume")*/}
+                {/*this.render_link("/slides", 3, "Slides")*/}
+                {/*this.render_link("/blog", 4, "Blog")*/}
+                {/*this.render_link("/projects", 5, "Projects")*/}
+              </Nav>
+            </Navbar>
         )
     }
 
-    render_link(path, key, text) {
-        return (
-            <LinkContainer to={path} exact activeClassName="active">
-                <NavItem href={path} eventKey={key}>{text}</NavItem>
-            </LinkContainer>
-        )
-    }
+  render_link(path, key, text) {
+    return (
+      <li to={path}>
+        <a href={path} eventKey={key}>{text}</a>
+      </li>
+    )
+  }
 }
 
-const StyledNavbar = styled(Navbar)`
-	background: #32383E;
-	border-top: #259d07 4px solid !important;
+const Navbar = styled.div`
+  background: #32383E;
+  border-top: #259d07 4px solid !important;
+  height: 60px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1030;
 `
 
-const StyledNavbarToggle = styled(Navbar.Toggle)`
-	background: #FFFFFF !important;
+const Nav = styled.ul`
+  display: flex;
 
-    .icon-bar {
-        background: #32383E;
-    }
-`
+  li > a {
+      font-weight: bold;
+  }
 
-const StyledNav = styled(Nav)`
-    li > a {
-        font-weight: bold;
-    }
+  .active,
+  li > a:focus {
+      color: #3AAA64;
+  }
 
-    .active,
-    li > a:focus {
-        color: #3AAA64;
-    }
-
-    .active,
-    li > a:hover,
-    li > a:focus {
-        background: #FFFFFF;
-    }
+  .active,
+  li > a:hover,
+  li > a:focus {
+      background: #FFFFFF;
+  }
 `
---
+++ src/components/navigation.story.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import {storiesOf} from '@storybook/react'
+import {Navigation} from './navigation'
+
+storiesOf('Navigation', module)
+  .add('default', () => <Navigation />)
