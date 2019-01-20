aa85661a37123606ccced7b10774a04a0ad9f397
Daniel Hess <dan9186@gmail.com>
2019-01-19 19:49:10 -0800

    formatting and fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,80 +12,89 @@ const PATHS = {
 }
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        main: './src/index.jsx',
-    },
-    module: {
-        rules: [
-            {
-                test: [/\.js$/, /\.jsx?$/],
-                exclude: [/node_modules\/(?!redux-form)/, /\.story.js$/, /\.test.js$/],
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.(png|jpg)$/,
-                loader: 'url-loader?limit=8192',
-            },
-            {
-                test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)|\.otf($|\?)/,
-                loader: "url-loader?limit=8192",
-            }
-        ],
-    },
-    output: {
-        path: PATHS.dist,
-        publicPath: '/',
-        filename: '[name].js',
-        chunkFilename: '[name].js',
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-            maxInitialRequests: 5,
-            cacheGroups: {
-                style: {
-                    test: /(styled-components|stylis)/,
-                    priority: 10,
-                },
-            },
+  mode: 'development',
+  entry: {
+    main: './src/index.jsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules\/(?!redux-form)/, /\.story.js$/, /\.test.js$/],
+        resolve: {
+          extensions: [".js", ".jsx"]
         },
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: './dist',
-        historyApiFallback: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: PATHS.src + '/index.ejs',
-            title: 'Daniel Hess',
-        }),
-        new FaviconsWebpackPlugin({
-            logo: PATHS.src + '/assets/images/logo-lg.png',
-            icons: {
-                android: true,
-                appleIcon: true,
-                appleStartup: false,
-                coast: false,
-                favicons: true,
-                firefox: true,
-                opengraph: false,
-                twitter: false,
-                yandex: false,
-                windows: false,
-            },
-        }),
-        new RobotstxtPlugin({
-            policy: [{
-                userAgent: "*",
-                disallow: "/",
-            }],
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production'),
-            VERSION: JSON.stringify(require("./package.json").version),
-            BUILD: JSON.stringify(process.env.TRAVIS_COMMIT || 'dev'),
-        })
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: {
+          loader: 'url-loader?limit=8192',
+        },
+      },
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)|\.otf($|\?)/,
+        use: {
+          loader: "url-loader?limit=8192",
+        },
+      },
     ],
+  },
+  output: {
+    path: PATHS.dist,
+    publicPath: '/',
+    filename: '[name].js',
+    chunkFilename: '[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: 5,
+      cacheGroups: {
+        style: {
+          test: /(styled-components|stylis)/,
+          priority: 10,
+        },
+      },
+    },
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: PATHS.src + '/index.ejs',
+      title: 'Daniel Hess',
+    }),
+    new FaviconsWebpackPlugin({
+      logo: PATHS.src + '/assets/images/logo-lg.png',
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false,
+      },
+    }),
+    new RobotstxtPlugin({
+      policy: [{
+        userAgent: "*",
+        disallow: "/",
+      }],
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      VERSION: JSON.stringify(require("./package.json").version),
+      BUILD: JSON.stringify(process.env.TRAVIS_COMMIT || 'dev'),
+    })
+  ],
 }
