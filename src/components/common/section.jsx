0904693d2111f8c40ee725c96d1f1de05e357d0f
Daniel Hess <dan9186@gmail.com>
2019-01-21 22:49:25 -0800

    creating a common usable section

---
+++ src/components/common/section.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export class Section extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      grow: this.props.grow || 1,
+    }
+  }
+
+  render() {
+    return (
+      <SectionContainer grow={this.state.grow}>
+        {this.props.children}
+      </SectionContainer>
+    )
+  }
+}
+
+export class Row extends React.Component {
+  render() {
+    return (
+      <RowContainer>
+        {this.props.children}
+      </RowContainer>
+    )
+  }
+}
+
+const RowContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: stretch;
+`
+
+const SectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: ${state => state.grow};
+  flex-basis: 0;
+
+  padding: 30px;
+  margin: 30px 15px 0px 15px;
+
+  background: #FFFFFF;
+  border-radius: 5px;
+  -moz-border-radius: 5px;
+  -webkit-border-radius: 5px;
+  border: 2px solid #778492;
+
+  color: #434343;
+  font-family: 'Lato', arial, sans-serif;
+  font-size: 16px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  a {
+    color: #3aaa64;
+    -webkit-transition: all .4s ease-in-out;
+    -moz-transition: all .4s ease-in-out;
+    -ms-transition: all .4s ease-in-out;
+    -o-transition: all .4s ease-in-out;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+    color: #616b76;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin-top: 0;
+    margin-bottom: 30px;
+    color: #545e69;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  h2 {
+    font-size: 24px;
+  }
+
+  h3 {
+    font-size: 22px;
+  }
+
+  h4 {
+    font-size: 18px;
+  }
+
+  h5 {
+    font-size: 14px;
+  }
+
+  h6 {
+    font-size: 10px;
+  }
+
+  hr {
+      border: #C2EAD2 1px solid;
+      margin: 0 0 20px 0;
+  }
+`
