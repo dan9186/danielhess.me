91c856a0a3729ad080951ee94b93ef6a97ca49b1
Daniel Hess <dan9186@gmail.com>
2019-05-02 22:24:14 -0700

    updating deps

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,12 @@ const webpack = require('webpack')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const RobotstxtPlugin = require('robotstxt-webpack-plugin').default
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 
 const PATHS = {
-    dist: path.join(__dirname, 'dist'),
-    src: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist'),
+  src: path.join(__dirname, 'src'),
 }
 
 module.exports = {
@@ -95,6 +97,10 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify('production'),
       VERSION: JSON.stringify(require("./package.json").version),
       BUILD: JSON.stringify(process.env.TRAVIS_COMMIT || 'dev'),
-    })
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled',
+      generateStatsFile: true,
+    }),
   ],
 }
