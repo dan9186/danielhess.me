1f4fbed5580c2b1e61912b3b3569a40236cf60f8
Daniel Hess <dan9186@gmail.com>
2019-08-18 20:50:59 -0700

    updating react, babel, and storybook

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = function (api) {
+  const presets = [
+    '@babel/preset-env',
+    '@babel/preset-flow',
+    '@babel/preset-react',
+  ]
+
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-runtime',
+    'babel-plugin-styled-components',
+  ]
+
+  if (api.env('test')) {
+    plugins.push('babel-plugin-require-context-hook')
+  }
+
+  return {
+    presets,
+    plugins,
+  }
+}
