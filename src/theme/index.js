221e2176e34bb9fc531f3b4af3866d48ba01b5e5
Daniel Hess <dan9186@gmail.com>
2020-01-29 21:32:28 -0800

    breaking theme up into more pieces for ease

---
+++ src/theme/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
+
+import { borderRadius } from './border_radius'
+import { media } from './media'
+import { palette } from './palette/'
+import { spacing } from './spacing'
+
+const theme = {
+  borderRadius,
+  media,
+  palette,
+  spacing,
+}
+
+export const Theme = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {children}
+  </ThemeProvider>
+)
+
+const GlobalStyle = createGlobalStyle`
+  html, body, #app {
+    display: flex;
+    flex: 1 0 auto;
+    height: 100%;
+    margin: ${props => props.theme.spacing(0)};
+    font-family: 'Lato', arial, sans-serif;
+    background-color: #26262d;
+  }
+
+  #app {
+    justify-content: center;
+    align-items: center;
+  }
+`
---
+++ src/theme/media.js
@@ -0,0 +1,11 @@
+export const media = {
+  landscape: `(orientation: landscape)`,
+  width: {
+    xss: `(max-width: 399px)`,
+    xs: `(min-width: 400px)`,
+    sm: `(min-width: 700px)`,
+    md: `(min-width: 900px)`,
+    lg: `(min-width: 1200px)`,
+    xl: `(min-width: 1800px)`,
+  },
+}
