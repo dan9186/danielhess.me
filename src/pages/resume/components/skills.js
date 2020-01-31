f6d1aaa5ec993e0b35b17fac59dacfa529db9c33
Daniel Hess <dan9186@gmail.com>
2020-01-31 01:46:47 -0800

    allow hiding skills section

--- src/pages/resume/components/skills.js
+++ src/pages/resume/components/skills.js
@@ -1,26 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faListAlt } from '@fortawesome/free-regular-svg-icons'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 import { Icon } from './icon'
 import { Section } from './section'
 import { SkillCategory } from './skill_category'
 import { Subtitle } from './subtitle'
 
-export const Skills = ({ skills = [] }) => (
-  <Section>
-    <Subtitle>
-      <Icon icon={faListAlt} />
-      Skills
-    </Subtitle>
-    <Content>
-      {skills.map(({ category, entries }) => (
-        <SkillCategory title={category} entries={entries} />
-      ))}
-    </Content>
-  </Section>
-)
+export const Skills = ({ skills = [] }) => {
+  const [show, setShow] = useState(false)
+
+  const handleClick = e => {
+    setShow(!show)
+    const target = e.target
+
+    if (!show) {
+      setTimeout(
+        () =>
+          target.scrollIntoView({
+            behavior: 'smooth',
+          }),
+        305,
+      )
+    }
+  }
+
+  return (
+    <Section>
+      <Subtitle onClick={handleClick}>
+        <Icon icon={faListAlt} />
+        Skills
+        <Chevron icon={faChevronRight} show={show} />
+      </Subtitle>
+      <Content show={show}>
+        {skills.map(({ category, entries }) => (
+          <SkillCategory title={category} entries={entries} />
+        ))}
+      </Content>
+    </Section>
+  )
+}
 
 const Content = styled.div`
   display: flex;
@@ -28,4 +50,19 @@ const Content = styled.div`
   margin-top: ${props => props.theme.spacing(2)};
   margin-bottom: ${props => props.theme.spacing(0)};
   padding-left: ${props => props.theme.spacing(2)};
+
+  overflow: hidden;
+  max-height: ${props => (props.show ? '10000px' : '0')};
+  opacity: ${props => (props.show ? '1' : '0')};
+  transition: all 300ms linear;
+`
+
+const Chevron = styled(FontAwesomeIcon)`
+  color: ${props => props.theme.palette.green[400]};
+  font-size: 16px;
+  padding-left: ${props => props.theme.spacing(1)};
+  padding-right: ${props => props.theme.spacing(1)};
+
+  transform: rotate(${props => (props.show ? '90deg' : '0')});
+  transition: all 300ms;
 `
