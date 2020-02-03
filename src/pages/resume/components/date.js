8b9e079dd58bc08ba75ac5213e2ff565906e0dc3
Daniel Hess <dan9186@gmail.com>
2020-02-02 22:25:23 -0800

    add time spent calc to date

--- src/pages/resume/components/date.js
+++ src/pages/resume/components/date.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
+import moment from 'moment'
 
-export const Date = ({ start, end }) => (
-  <Container active={end.toLowerCase() === 'present'}>
-    {start} - {end}
-  </Container>
-)
+export const Date = ({ start, end }) => {
+  const active = end.toLowerCase() === 'present'
+
+  const stop = active ? moment.now() : end
+  const durr = moment.duration(moment(stop).diff(moment(start)))
+  const years = durr.years()
+  const months = durr.months()
+
+  const timeSpent = years > 0 ? `${years}y ${months}mo` : `${months}mo`
+
+  return (
+    <Container>
+      <Pill active={active}>
+        {start} - {end}
+      </Pill>
+      <Pill active={active}>{timeSpent}</Pill>
+    </Container>
+  )
+}
 
 const Container = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+`
+
+const Pill = styled.div`
   max-width: fit-content;
   border: 1px solid ${props =>
     props.active
@@ -15,6 +35,7 @@ const Container = styled.div`
       : props.theme.palette.grey[500]};
   ${props => props.theme.borderRadius.all}
   margin-top: ${props => props.theme.spacing(1)};
+  margin-right: ${props => props.theme.spacing(1)};
   padding-right: ${props => props.theme.spacing(0.5)};
   padding-left: ${props => props.theme.spacing(0.5)};
 
