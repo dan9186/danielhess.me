7e51a4fc4b0d9f0534538d831be0478c4dd12d56
Daniel Hess <dan9186@gmail.com>
2020-02-18 23:20:52 -0800

    fixing broken story with inclusion of theme

--- src/components/card/card.story.js
+++ src/components/card/card.story.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { StoryTheme } from '../../theme'
 import { Card } from './card'
 
-storiesOf('Components/Card', module).add('default', () => <Card />)
+storiesOf('Components/Card', module)
+  .addDecorator(StoryTheme)
+  .add('default', () => <Card />)
