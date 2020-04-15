0df5a79c862d354c50e62986d000a96e20ae17d3
Daniel Hess <dan9186@gmail.com>
2020-04-14 21:33:58 -0700

    updating to change based on active status

--- src/components/menu/icon.js
+++ src/components/menu/icon.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const Icon = styled(FontAwesomeIcon)`
-  color: ${({ theme }) => theme.palette.green[400]};
   font-size: 24px;
   padding-left: ${({ theme }) => theme.spacing(1)};
   padding-right: ${({ theme }) => theme.spacing(1)};
