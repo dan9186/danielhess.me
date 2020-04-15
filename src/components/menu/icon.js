8e8a76b3a9b5fff4d65fecbae6892777b38cd665
Daniel Hess <dan9186@gmail.com>
2020-04-14 21:09:05 -0700

    fixing menu shape

--- src/components/menu/icon.js
+++ src/components/menu/icon.js
@@ -6,4 +6,5 @@ export const Icon = styled(FontAwesomeIcon)`
   font-size: 24px;
   padding-left: ${({ theme }) => theme.spacing(1)};
   padding-right: ${({ theme }) => theme.spacing(1)};
+  margin-bottom: ${({ theme }) => theme.spacing(1)};
 `
--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
-import { faFileAlt, faCommentDots } from '@fortawesome/free-solid-svg-icons'
+import {
+  faFileAlt,
+  faCommentDots,
+  faPaintBrush,
+} from '@fortawesome/free-solid-svg-icons'
 
 import { Item } from './item.js'
 
 export const Menu = () => (
   <Container>
     <Items>
-      <Item text='Resume' to='/resume' icon={faFileAlt} />
-      <Item text='Projects' to='/project' />
+      <Item text='Resume' to='/' icon={faFileAlt} />
+      <Item text='Projects' to='/project' icon={faPaintBrush} />
       <Item text='Blog' to='/blog' icon={faCommentDots} />
     </Items>
   </Container>
 )
 
 const Container = styled.div`
-  height: 100%;
+  z-index: 2;
+
+  @media ${({ theme }) => theme.media.width.md} {
+    margin-right: ${({ theme }) => theme.spacing(1)};
+  }
 `
 
 const Items = styled.ul`
   display: flex;
   flex-flow: row wrap;
-  max-width: 72px;
   margin: 0;
   padding: 0;
 
@@ -29,4 +36,8 @@ const Items = styled.ul`
   ${({ theme }) => theme.borderRadius.all}
 
   list-style: none;
+
+  @media ${({ theme }) => theme.media.width.md} {
+    max-width: 72px;
+  }
 `
