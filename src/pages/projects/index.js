d408c142b5b0c6f9507025bc6e05c7c1b2a43ebe
Daniel Hess <dan9186@gmail.com>
2020-04-15 00:58:07 -0700

    adding stubbed pages

---
+++ src/pages/projects/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { faHammer } from '@fortawesome/free-solid-svg-icons'
+
+import { Row, Section } from '../../components/grid'
+import { Icon, Title } from '../../components/typography'
+
+export const Projects = () => (
+  <Container>
+    <Title>
+      <div>
+        <Icon icon={faHammer} />
+        Projects
+      </div>
+    </Title>
+    <Row>
+      <Section>
+        <Content>foo</Content>
+      </Section>
+    </Row>
+  </Container>
+)
+
+const Container = styled.div`
+  display: flex;
+  flex: 1 0 auto;
+  flex-flow: column;
+  max-width: fit-content;
+
+  color: #a2a2a2;
+  font-weight: 300;
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  margin-top: ${({ theme }) => theme.spacing(2)};
+  margin-bottom: ${({ theme }) => theme.spacing(0)};
+  padding-left: ${({ theme }) => theme.spacing(2)};
+`
