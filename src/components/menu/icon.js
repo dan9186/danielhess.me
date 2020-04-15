3996344fd830998e19fec8b6444a17145537d026
Daniel Hess <dan9186@gmail.com>
2020-04-14 19:40:12 -0700

    getting there, time to merge

---
+++ src/components/menu/icon.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export const Icon = styled(FontAwesomeIcon)`
+  color: ${({ theme }) => theme.palette.green[400]};
+  font-size: 24px;
+  padding-left: ${({ theme }) => theme.spacing(1)};
+  padding-right: ${({ theme }) => theme.spacing(1)};
+`
---
+++ src/components/menu/item.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+import { Icon } from './icon'
+
+export const Item = ({ text, to, icon }) => (
+  <Container to={to}>
+    <ListItem>
+      {icon && <Icon icon={icon} />}
+      <Text>{text}</Text>
+    </ListItem>
+  </Container>
+)
+
+const Container = styled(Link)`
+  height: 69px;
+  width: 72px;
+
+  text-decoration: none;
+`
+
+const ListItem = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+  flex: 1;
+  margin: 0;
+  padding: 0;
+`
+
+const Text = styled.span`
+  width: 100%;
+  text-align: center;
+
+  color: ${({ theme }) => theme.palette.grey[300]};
+  font-size: 11px;
+`
